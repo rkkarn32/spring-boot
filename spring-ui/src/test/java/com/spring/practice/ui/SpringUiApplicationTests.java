f043ccabea884f70c075f722263e7f35c18a0889
Ramesh Karan <rk.karn32@gmail.com>
2020-06-11 22:38:34 -0400

    Back end added for Spring-UI project

---
+++ spring-ui/src/test/java/com/spring/practice/ui/SpringUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.practice.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
